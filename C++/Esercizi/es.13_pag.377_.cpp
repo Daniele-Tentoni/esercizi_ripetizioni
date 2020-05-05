1bb871a288cffec61d3163076129b78f86069290
MolaraMarco <mola.marco.02@gmail.com>
2020-05-05 19:38:08 +0200

    es.13_pag.377 C++

---
+++ C++/Esercizi/es.13_pag.377_.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+	float t =20;
+	float g =9.81;
+	
+	float h = g*t*t/2;
+	 
+	cout << "il corpo e' caduto da una altezza di:  " ;
+	cout << h; 
+	cout << " metri\n";
+	
+	
+	
+	
+	
+	
+	
+}
