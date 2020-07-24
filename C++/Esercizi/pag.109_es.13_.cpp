1740efedfc61d692151a3efe49f2979b33373052
MolaraMarco <mola.marco.02@gmail.com>
2020-07-24 09:11:46 +0200

    pag.109 es.13 (corretto) C++

--- C++/Esercizi/pag.109_es.13_.cpp
+++ C++/Esercizi/pag.109_es.13_.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <cmath>
 using namespace std ;
 
 int funzionePotenza ( int base, int esponente)
 {
 //	int risultato ;
+
+	int baseFissa = base ;
 	for ( int i = 0 ; i < esponente-1 ; i++)
 	{
 		if ( i == 0 )
-		base = base * base ;
+		base = base * baseFissa ;
 		if ( i != 0 )
-		base = base * sqrt(base) ;
+		base = base * baseFissa ;
 	}
 	
 	return base ;
